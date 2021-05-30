2fdd7fe352741268531624410532b0e9f7858669
Mahmoud Saadeh <mahmoud.saadeh998@gmail.com>
2021-05-30 23:26:00 +0300

    all this without srapi for the backend

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,29 @@
+import EventItem from '@/components/EventItem';
+import Link from 'next/link';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function EventsPage() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
       <h1>Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
+}
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events: events.slice(0, 3) },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import EventItem from '@/components/EventItem';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   );
+}
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
