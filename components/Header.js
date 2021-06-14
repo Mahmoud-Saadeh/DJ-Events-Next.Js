11c909be8a1a0eed1436f80fe8199e5a0f8132b0
Mahmoud-Saadeh <mahmoud.saadeh998@gmail.com>
2021-06-14 23:44:15 +0300

    authentication

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
+import { useContext } from 'react';
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -18,11 +22,40 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
+          {user ? (
+            // if logged in
+            <>
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className="btn-secondary btn-icon"
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // if logged out
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
---
+++ pages/api/register.js
@@ -0,0 +1,42 @@
+import { API_URL } from '@/config/index';
+import cookie from 'cookie';
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { userName, email, password } = req.body;
+
+    const strapiRes = await fetch(`${API_URL}/auth/local/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: userName,
+        email,
+        password,
+      }),
+    });
+    const data = await strapiRes.json();
+
+    if (strapiRes.ok) {
+      //Set Cookie
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('token', data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          maxAge: 60 * 60 * 24 * 7, //1 week
+          sameSite: 'strict',
+          path: '/', // make it accessible from everywhere
+        })
+      );
+      res.status(200).json({ user: data.user });
+    } else {
+      res
+        .status(data.statusCode)
+        .json({ message: data.message[0].messages[0].message });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).json({ message: `Method ${req.method} not allowed` });
+  }
+};
